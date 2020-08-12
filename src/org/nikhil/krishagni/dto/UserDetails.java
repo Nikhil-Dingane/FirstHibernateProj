574613605ae6ee6c701a2d3ac3d40f4fc2572be0
Nikhil Dingane <nikhil@krishagni.com>
2020-08-12 15:17:30 +0530

    used @GeneratedValue annotation to generate primary key

--- src/org/nikhil/krishagni/dto/UserDetails.java
+++ src/org/nikhil/krishagni/dto/UserDetails.java
@@ -1,16 +1,26 @@
 package org.nikhil.krishagni.dto;
 
+import java.util.Date;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.Lob;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.persistence.Transient;
 
-@Entity(name = "USER_DETAILS")
+@Entity
+@Table(name = "USER_DETAILS")
 public class UserDetails {
-	@Id
-	@Column(name = "USER_ID")
+	@Id @GeneratedValue
 	private int userId;
-	@Column(name = "USER_NAME")
 	private String userName;
+	private Date joiningDate;
+	private String address;
+	private String description;
 	
 	public int getUserId() {
 		return userId;
@@ -23,6 +33,24 @@ public class UserDetails {
 	}
 	public void setUserName(String userName) {
 		this.userName = userName;
+	}
+	public Date getJoiningDate() {
+		return joiningDate;
+	}
+	public void setJoiningDate(Date joiningDate) {
+		this.joiningDate = joiningDate;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
 	}
 	
 	
--- src/org/nikhil/krishagni/hibernate/HibernateTest.java
+++ src/org/nikhil/krishagni/hibernate/HibernateTest.java
@@ -1,5 +1,7 @@
 package org.nikhil.krishagni.hibernate;
 
+import java.util.Date;
+
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
@@ -10,13 +12,26 @@ public class HibernateTest {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		UserDetails user = new UserDetails();
-		user.setUserId(2);
-		user.setUserName("Second User");
+		//user.setUserId(1);
+		user.setUserName("First User");
+		
+		UserDetails user2 = new UserDetails();
+		user2.setUserName("Second User");
+
+		
 		SessionFactory sessionFactory = new Configuration().configure().buildSessionFactory();
+		
 		Session session = sessionFactory.openSession();
 		session.beginTransaction();;
 		session.save(user);
+		session.save(user2);
 		session.getTransaction().commit();
-	}
+		session.clear();
+		
+		/*
+		 * session = sessionFactory.openSession(); session.beginTransaction(); user =
+		 * (UserDetails) session.get(UserDetails.class,1);
+		 * System.out.println("Username of the user is : " + user.getUserName());
+		 */	}
 
 }
