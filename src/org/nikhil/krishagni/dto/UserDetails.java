470e0d2a780fd3461344460dd21abf9e9ae2a7b1
Nikhil Dingane <nikhil@krishagni.com>
2020-08-17 11:18:00 +0530

    configuring collections and adding object keys

--- src/org/nikhil/krishagni/dto/UserDetails.java
+++ src/org/nikhil/krishagni/dto/UserDetails.java
@@ -1,5 +1,7 @@
 package org.nikhil.krishagni.dto;
 
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.Date;
 import java.util.HashSet;
 import java.util.Set;
@@ -13,11 +15,19 @@ import javax.persistence.Embedded;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinColumns;
+import javax.persistence.JoinTable;
 import javax.persistence.Lob;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 import javax.persistence.Transient;
+
+import org.hibernate.annotations.CollectionId;
+import org.hibernate.annotations.GenericGenerator;
+import org.hibernate.annotations.Type;
+
 
 @Entity
 @Table(name = "USER_DETAILS")
@@ -26,19 +36,22 @@ public class UserDetails {
 	private int userId;
 	private String userName;
 	@ElementCollection
-	private Set<Address> setOfAddresses = new HashSet<Address>();
+	@JoinTable(name = "USER_ADDRESS", joinColumns = @JoinColumn(name= "USER_ID"))
+	@GenericGenerator(name = "hilo-gen", strategy = "hilo")
+	@CollectionId(columns = { @Column(name = "Address_ID") }, generator = "hilo-gen", type = @Type(type = "long"))
+	private Collection<Address> setOfAddresses = new ArrayList<Address>();
 	/*
 	 * @Embedded private Address address;
 	 */
 	
-	public Set<Address> getSetOfAddresses() {
-		return setOfAddresses;
-	}
-	public void setSetOfAddresses(Set<Address> setOfAddresses) {
-		this.setOfAddresses = setOfAddresses;
-	}
 	public int getUserId() {
 		return userId;
+	}
+	public Collection<Address> getSetOfAddresses() {
+		return setOfAddresses;
+	}
+	public void setSetOfAddresses(Collection<Address> setOfAddresses) {
+		this.setOfAddresses = setOfAddresses;
 	}
 	public void setUserId(int userId) {
 		this.userId = userId;
