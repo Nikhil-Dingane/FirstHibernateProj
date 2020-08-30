74783c2a87cca81b39f4070ae790e2dc3c196ccd
Nikhil Dingane <nikhil@krishagni.com>
2020-08-30 16:05:30 +0530

    understanding caching in Hibernate(default caching)

--- src/org/nikhil/krishagni/hibernate/HibernateTest.java
+++ src/org/nikhil/krishagni/hibernate/HibernateTest.java
@@ -1,6 +1,5 @@
 package org.nikhil.krishagni.hibernate;
 
-
 import java.text.CharacterIterator;
 import java.util.Collection;
 import java.util.List;
@@ -12,6 +11,7 @@ import org.hibernate.Query;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
+import org.hibernate.criterion.Example;
 import org.hibernate.criterion.Order;
 import org.hibernate.criterion.Projections;
 import org.hibernate.criterion.Restrictions;
@@ -20,41 +20,34 @@ import org.nikhil.krishagni.dto.TwoWheeler;
 import org.nikhil.krishagni.dto.UserDetails;
 import org.nikhil.krishagni.dto.Vehicle;
 
-
 public class HibernateTest {
 
 	public static void main(String[] args) {
-			
-		
+
 		SessionFactory sessionFactory = new Configuration().configure().buildSessionFactory();
-		
+
 		Session session = sessionFactory.openSession();
 		session.beginTransaction();
-		
-		
-		/*
-		 * Query query= session.getNamedQuery("UserDetails.byName");
-		 * query.setString(0,"User6");
-		 */
-		
-		Criteria criteria = session.createCriteria(UserDetails.class);
-		//criteria.addOrder(Order.desc("userId"));
-		criteria.setProjection(Projections.property("userId"));
-		
-		List<Integer> users1 = (List<Integer>)criteria.list();
 
-		for (int i : users1) {
-			System.out.println(i);
-		}
+		UserDetails user = (UserDetails)session.get(UserDetails.class,2);
+		System.out.println(user.getUserName());
+		user.setUserName("Second user");
+		System.out.println(user.getUserName());
+
+		UserDetails user2 = (UserDetails)session.get(UserDetails.class,2);
+		System.out.println(user2.getUserName());
+
+
+		//List<UserDetails> users = (List<UserDetails>) criteria.list();
+
+		session.getTransaction().commit();
+		session.clear();
+
 		/*
-		 * List<UserDetails> users = (List<UserDetails>)criteria.list();
-		 * 
-		 * session.getTransaction().commit(); session.clear();
-		 * 
-		 * 
-		 * for(UserDetails user:users) { System.out.println(user.getUserId() + "\t"
-		 * +user.getUserName()); }
+		 * for (UserDetails user : users) { System.out.println(user.getUserId() + "\t" +
+		 * user.getUserName()); }
 		 */
+
 	}
 
 }
